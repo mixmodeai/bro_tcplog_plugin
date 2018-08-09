019a15990d7806fb95245b1b2e0bfafe4d8ead6a
Chris_Hinshaw <chris.hinshaw@packetsled.com>
2018-08-09 15:17:32 -0700

    Change leading 0 formatting, again

--- parse_profile.sh
+++ parse_profile.sh
@@ -2,7 +2,7 @@
 grep WRITER_TCPLOG /usr/local/bro/spool/logger/std*.log | sed 's/.*log://g' | sed 's/\/.*range /,/g' >logger_stderr.log
 for size in `cat logger_stderr.log | tr ',' ' ' | awk '{print $2}' | sort -u` ; 
 do 
-    echo Size $size count
+    echo Size $(echo $size | sed 's/^0*//') count
     echo $(for type in `cat logger_stderr.log | sed 's/,.*//g' | sort -u` ; 
     do 
         grep $type logger_stderr.log | grep $size | tail -1 | tr ',' ' ' | awk '{print $3"+"}'; 
